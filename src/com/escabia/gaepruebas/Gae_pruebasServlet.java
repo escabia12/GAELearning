37145fc68ab604d0155c7d0a0e63ef7c604765af
Ruben Escabia <escabia12@gmail.com>
2015-10-18 17:52:00 +0200

    First commit, hello world

---
+++ src/com/escabia/gaepruebas/Gae_pruebasServlet.java
@@ -0,0 +1,12 @@
+package com.escabia.gaepruebas;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Gae_pruebasServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
